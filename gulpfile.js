084f011768fb35f03b0a1d331f450a9baf5cd951
Vladimir <getskowork@gmail.com>
2021-01-12 10:06:26 +0200

    Added startup build files

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss 					= require('gulp-sass'),
+			concat 				= require('gulp-concat'),
+			browserSync 	= require('browser-sync').create(),
+			uglify 				= require('gulp-uglify-es').default,
+			autoprefixer 	= require('gulp-autoprefixer'),
+			imagemin 			= require('gulp-imagemin'),
+			del 					= require('del');
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: "app/"
+		}
+	});
+}
+
+function cleanDist() {
+	return del('dist');
+}
+
+function images () {
+	return src('app/images/**/*')
+					.pipe(imagemin(
+						[
+							imagemin.gifsicle({ interlaced: true }),
+							imagemin.mozjpeg({ quality: 75, progressive: true }),
+							imagemin.optipng({ optimizationLevel: 5 }),
+							imagemin.svgo({
+								plugins: [
+									{ removeViewBox: true },
+									{ cleanupIDs: false }
+								]
+							})
+						]
+					))
+					.pipe(dest('dist/images'));
+}
+
+function scripts() {
+	return src([
+						'node_modules/jquery/dist/jquery.js',
+						'app/js/main.js'
+					])
+					.pipe(concat('main.min.js'))
+					.pipe(uglify())
+					.pipe(dest('app/js'))
+					.pipe(browserSync.stream());
+}
+
+function styles() {
+	return 	src('app/scss/style.scss')
+					.pipe(scss({outputStyle: 'compressed'})) // compressed || expanded
+					.pipe(concat('style.min.css'))
+					.pipe(autoprefixer({
+						overrideBrowserslist: ['last 10 version'],
+						grid: true
+					}))
+					.pipe(dest('app/css'))
+					.pipe(browserSync.stream());
+}
+
+function build() {
+	return src([
+						'app/css/style.min.css',
+						'app/css/**/*',
+						'app/js/main.min.js',
+						'app/*.html'
+					], {base: 'app'}) // base directory
+					.pipe(dest('dist'));
+}
+
+function watching() {
+	watch(['app/scss/**/*.scss'], styles);
+	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+	watch(['app/*.html']).on('change', browserSync.reload);
+}
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.images = images;
+exports.cleanDist = cleanDist;
+
+exports.build = series(cleanDist, images, build);
+exports.default = parallel(scripts, styles, browsersync, watching, scripts);
+
